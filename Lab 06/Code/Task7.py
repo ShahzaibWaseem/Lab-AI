f5e82d000511f8804cbab2b214c4cf417df354e3
ShahzaibWaseem <shahzaib.waseem96@gmail.com>
2019-04-19 22:05:20 +0500

    Removing Task7.py

--- Lab 06/Code/Task7.py
+++
@@ -1,6 +0,0 @@
-from math import atan2, degrees
-"""
-Task 7: Angle of Inclination
-"""
-def AngleOfInclination(image, left, right, top, bottom, cx, cy):
-	return abs(degrees(atan2(bottom - top - cy, right - left - cx)))
